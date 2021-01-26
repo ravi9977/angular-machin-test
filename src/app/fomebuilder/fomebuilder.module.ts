8f43a87d98cc4e36cf3d12f91c22e98eba65fd63
ravi <ravi.rathore@graffersid.com>
2021-01-26 17:23:24 +0530

    initial commit

---
+++ src/app/fomebuilder/fomebuilder.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FomebuilderRoutingModule } from './fomebuilder-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FomebuilderRoutingModule
+  ]
+})
+export class FomebuilderModule { }
